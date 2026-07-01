16e9aa4ca94a9f78b9ce1f17ac5b015e8b12ee04
AARUSHI BISHT <154417633+aarushibisht01@users.noreply.github.com>
2026-07-01 20:55:56 +0530

    data cleaning done

--- model.py
+++ model.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,r2_score
 
@@ -38,4 +41,33 @@ print("For categorical columns:\n")
 print(df.describe(include="object"))
 
 print("Number of missing values:\n")
-print(df.isnull().sum().sum())
+print(df.isnull().sum())
+
+#Data Cleaning
+
+#removing duplicate rows
+df=df.drop_duplicates()
+
+#removing missing values
+df=df.dropna()
+
+#Data Preprocessing
+#seperating features and target
+X=df.drop(collumn="price",axis=1)
+y=df["price"]
+
+#encoding categorical features for data preprocessing
+column_transformer=ColumnTransformer(
+    transformers=[("encoding",OneHotEncoder(),["street","city","statezip","country"])],
+    remaindr="passthrough"
+)
+
+X=column_transformer.fit_transform(X)
+
+#training the data
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+#feature scaling to normalize the range of independent variables
+scaler=StandardScaler()
+X_train=scaler.fit_transform(X_train)
+y_train=scaler.fit_transform(y_train)
